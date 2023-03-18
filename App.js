548998f122e6878e42e322448835e2e1f9a14273
Joysurya Mondal <you@example.com>
2023-03-18 19:31:55 +0600

    6

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetUp";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        {/* <Route path="/">
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route> */}
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
