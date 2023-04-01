92e90aca7ffd83ddb02a20a8648942f305b37704
Joysurya Mondal <you@example.com>
2023-04-01 21:23:39 +0600

    13.3+13.4+13.5

--- pages/NewMeetUp.js
+++ pages/NewMeetUp.js
@@ -1,9 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetup-f2d10-default-rtdb.firebaseio.com/meetups.json", {
+      method: "Post",
+      body: JSON.stringify(meetupData),
+      Headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      navigate("/", { replace: true });
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
